e327487d13129ef0f9c323322cef5de4712abed5
Ham-Seung-Wan <syham001@naver.com>
2024-05-02 17:34:44 +0900

    Create branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        private static List<Student> studentStore;
+        private static List<Subject> subjectStore;
+        private static List<Score> scoreStore;
     }
-
-
 }
