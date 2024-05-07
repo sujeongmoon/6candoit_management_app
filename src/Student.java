baddf3eb590985cb9ae59f8a68d617d98de90001
이종원 <ejong_1@naver.com>
2024-05-07 16:42:32 +0900

    240507-3

--- src/Student.java
+++ src/Student.java
@@ -24,13 +24,6 @@ public class Student {
     //메서드
 
 
-    public List<Score> getScoreStores() {
-        return this.scoreStores;
-    }
-
-    public void addScoreStores(Score score) {
-       scoreStores.add(score);
-    }
     /*
     특정 점수 찾기 메서드
     */
@@ -56,21 +49,66 @@ public class Student {
     public String getStudentNum() {
         return studentNum;
     }
+
     /*
     이름 getter
      */
     public String getStudentName() {
         return studentName;
     }
+
     /*
 
-   */
+     */
     public List<Integer> getSubjects() {
         return Subjects;
     }
 
+    /*
+    수강생이 선택한 과목과 입력받은 과목이 일치하지 않는 경우를 검증
+     */
+    public boolean checkSubjectScore(int subjectNum) {
+        for (Integer subject : Subjects) {
+            if (subject == subjectNum) {
+                return true;
+            }
+        }
+        System.out.println("수강하지 않는 과목입니다.");
+        System.out.println("다시 입력해주세요.");
+        return false;
+    }
+
+    public void addScore(int scoreSubject, int score, int round) {
+
+            if (checkSubjectScore(scoreSubject)) {
+                this.score.createScore(scoreSubject, score, round);
+
+            }
+            checkSubjectScore(scoreSubject);
 
 
+    }
+
+
+    public void modScore(int scoreSubject, int score, int round) {
+
+            if (checkSubjectScore(scoreSubject)) {
+                this.score.modifyScore(scoreSubject, score, round);
+
+            }
+            checkSubjectScore(scoreSubject);
+
+    }
+
+
+    public void inqScore(int scoreSubject) { // 매개변수를 없앨 수 있는지
+
+            if (checkSubjectScore(scoreSubject)) {
+                this.score.inquiryScore(scoreSubject);
+            }
+            checkSubjectScore(scoreSubject);
+
+    }
 }
 
 
