4cc1af61cb06c8a5e00f0cbdf4aa176abd0d24bf
이종원 <ejong_1@naver.com>
2024-05-07 19:34:31 +0900

    240507-3

--- src/Student.java
+++ src/Student.java
@@ -10,39 +10,20 @@ public class Student {
     private String studentName;
     // 과목
     private List<Integer> Subjects;
-    //삭제
-    private Score score;
+
 
     //생성자
     public Student(String studentNum, String studentName, List<Integer> Subjects) {
         this.studentNum = studentNum;
         this.studentName = studentName;
         this.Subjects = Subjects;
-        score = new Score();
+
     }
 
     //메서드
 
 
-    /*
-    특정 점수 찾기 메서드
-    */
-    /*public Score searchGetScore( int subjectIdx, int roundNum) {
-        Score score = scoreStores.stream()
-                .filter(a -> a.getSubjectNum() == subjectIdx && a.getRoundNum() == roundNum)
-                .findFirst()
-                .orElseThrow(null);
-        System.out.println(score.getExamScore());
-        return score;
-    }
 
-    public List<Score> searchGetSore( int subjectIdx, int roundNum) {
-        List<Score> score = scoreStores.stream()
-                .filter(a ->  a.getRoundNum() == roundNum)
-                .toList();
-
-        return score;
-    }*/
     /*
     학번 getter
      */
@@ -64,51 +45,20 @@ public class Student {
         return Subjects;
     }
 
-    /*
-    수강생이 선택한 과목과 입력받은 과목이 일치하지 않는 경우를 검증
-     */
-    public boolean checkSubjectScore(int subjectNum) {
-        for (Integer subject : Subjects) {
-            if (subject == subjectNum) {
-                return true;
-            }
-        }
-        System.out.println("수강하지 않는 과목입니다.");
-        System.out.println("다시 입력해주세요.");
-        return false;
-    }
-
-    public void addScore(int scoreSubject, int score, int round) {
-
-            if (checkSubjectScore(scoreSubject)) {
-                this.score.createScore(scoreSubject, score, round);
-
-            }
-            checkSubjectScore(scoreSubject);
-
-
+    public Score getScore() {
+        return this.score;
     }
 
 
-    public void modScore(int scoreSubject, int score, int round) {
-
-            if (checkSubjectScore(scoreSubject)) {
-                this.score.modifyScore(scoreSubject, score, round);
-
-            }
-            checkSubjectScore(scoreSubject);
-
-    }
 
 
-    public void inqScore(int scoreSubject) { // 매개변수를 없앨 수 있는지
 
-            if (checkSubjectScore(scoreSubject)) {
-                this.score.inquiryScore(scoreSubject);
-            }
-            checkSubjectScore(scoreSubject);
 
-    }
+
+
+
+
+
 }
 
 
